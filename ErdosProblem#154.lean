13daef9efecde8883350edd0ae411b3ebaedc380
Wouter van Doorn <107570935+Woett@users.noreply.github.com>
2026-02-22 14:21:02 +0100

    Correct attribution

--- ErdosProblem#154.lean
+++ ErdosProblem#154.lean
@@ -5,7 +5,7 @@ Lindström, Bernt, Well distribution of Sidon sets in residue classes. J. Number
 
 This solves Erdős Problem #154 (https://www.erdosproblems.com/154).
 
-I asked ChatGPT to write up Lindström's proof in a TeX-file, which Aristotle subsequently formalized into Lean, the result of which you can find below. Thanks go to Borix Alexeev for cleaning up the code in order to get rid of all the warnings <3 Thank you!
+I asked ChatGPT to write up Lindström's proof in a TeX-file, which Aristotle from Harmonic (aristotle-harmonic@harmonic.fun) subsequently formalized into Lean, the result of which you can find below. Thanks go to Borix Alexeev for cleaning up the code in order to get rid of all the warnings <3 Thank you!
 
 Lean version: leanprover/lean4:v4.24.0
 Mathlib version: f897ebcf72cd16f89ab4577d0c826cd14afaafc7
@@ -1043,3 +1043,4 @@ theorem sidon_density_limit
     intro i hi; specialize h_v_const; rw [ tendsto_pi_nhds ] at h_v_const; specialize h_v_const ⟨ i, hi ⟩ ; aesop;
 
 #print axioms sidon_density_limit
+
