56d85891194dc89194577bbcb7226865603d0d6a
Wouter van Doorn <107570935+Woett@users.noreply.github.com>
2026-02-22 08:38:03 +0100

    Added sentence

--- ErdosProblem#355.lean
+++ ErdosProblem#355.lean
@@ -7,7 +7,7 @@ Below you can find a formalization of this result, with thanks to Boris Alexeev 
 
 As for the exact result that we will prove: even though the above paper shows that any lacunarity constant smaller than 2 is possible, the formalization below is based on a simplified proof that Vjeko wrote, which has lacunarity constant 1.01. This simplified proof was given to Gemini3 in order to make it more easily formalizable, which was eventually done with the use of Aristotle (and a whole lot of patience on my end).
 
-At the very end you can find the statement of Erdős Problem #355 taken from the Formal Conjectures project by Google DeepMind. 
+At the very end you can find the statement of Erdős Problem #355 taken from the Formal Conjectures project by Google DeepMind, which we also prove. 
 
 https://github.com/google-deepmind/formal-conjectures/blob/main/FormalConjectures/ErdosProblems/355.lean
 
@@ -1024,5 +1024,4 @@ theorem erdos_355 :
   rw [ Finset.sum_image <| by tauto ] ; norm_num [ ← @Rat.cast_inj ℝ, hT ] ;)))
 
 #print axioms maintheorem
-
 #print axioms erdos_355
