46c2bb08e3577baa57c9bc37a5d79e07feb3f1f9
Wouter van Doorn <107570935+Woett@users.noreply.github.com>
2026-04-19 11:46:38 +0200

    Update ErdosProblem485.lean

--- ErdosProblem485.lean
+++ ErdosProblem485.lean
@@ -5,7 +5,7 @@ Below you can find a formalization, obtained by Aristotle from Harmonic
 (aristotle-harmonic@harmonic.fun), of the following two theorems by Verdenius:
 
 **Theorem**. For every positive integer $n$ there exists a polynomial $f(x)$ of degree $n$ with all non-zero integer
-coefficients such that $f(x)²$ has fewer than $(1/5)(102 · n^{log₉ 6} - 12)$ non-zero coefficients.
+coefficients such that $f(x)²$ has fewer than $(1/5)(102 · n^{log_9 6} - 12)$ non-zero coefficients.
 
 **Theorem**. For every positive integer $n$, there exists a polynomial $f(x)$ of degree $n$ with all non-zero real
 coefficients such that $f(x)²$ has fewer than $(1/7)(170 · n^{log_{13} 8} - 14)$ non-zero coefficients.
