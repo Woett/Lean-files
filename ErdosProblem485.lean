28a3110fbdd1968b4dff2ad0b60f7efaac798c46
Wouter van Doorn <107570935+Woett@users.noreply.github.com>
2026-04-19 11:46:00 +0200

    Update theorem formatting to LaTeX style

--- ErdosProblem485.lean
+++ ErdosProblem485.lean
@@ -4,13 +4,11 @@ import Mathlib
 Below you can find a formalization, obtained by Aristotle from Harmonic
 (aristotle-harmonic@harmonic.fun), of the following two theorems by Verdenius:
 
-**Theorem**. For every positive integer `n`, there exists a polynomial `f(x)` of degree `n` with all nonzero integer
-coefficients such that `f(x)²` has fewer than `(1/5)(102 · n^{log₉ 6} - 12)`
-nonzero coefficients.
+**Theorem**. For every positive integer $n$ there exists a polynomial $f(x)$ of degree $n$ with all non-zero integer
+coefficients such that $f(x)²$ has fewer than $(1/5)(102 · n^{log₉ 6} - 12)$ non-zero coefficients.
 
-**Theorem**. For every positive integer `n`, there exists a polynomial `f(x)` of degree `n` with all nonzero real
-coefficients such that `f(x)²` has fewer than `(1/7)(170 · n^{log_{13} 8} - 14)`
-nonzero coefficients.
+**Theorem**. For every positive integer $n$, there exists a polynomial $f(x)$ of degree $n$ with all non-zero real
+coefficients such that $f(x)²$ has fewer than $(1/7)(170 · n^{log_{13} 8} - 14)$ non-zero coefficients.
 
 W. Verdenius, On the number of terms of the square and the cube of polynomials, Indag. Math. 11 (1949), 546–565.
 -/
